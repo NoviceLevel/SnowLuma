ec63dc0ab192c8c5726ec67b8a4435e22271d76f
NoviceLevel <62823553+NoviceLevel@users.noreply.github.com>
2026-08-10 19:48:45 +0800

    chore(plugin): 移除 Nico 插件

--- Plugin/Nico/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-cd "$(dirname "$0")"
-node_executable="node"
-if [ -x "./runtime/bin/node" ]; then
-  node_executable="./runtime/bin/node"
-fi
-exec "$node_executable" --env-file-if-exists=config.env index.mjs
